95a91ba340616cd136dfa6991cde1336dba3c758
Toeybai <89177693+Toeybai@users.noreply.github.com>
2021-09-02 10:47:59 +0700

    Update app.py

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ data = [
 
 @app.route('/')
 def hello():
-    return "Hello Flask-Herok"
+    return "Hello ภัคจิรา​ พันธ์พฤกษ์​ เลขที่​30 ม.4/6"
 
 @app.route('/api', methods=['GET'])
 def get_api():
